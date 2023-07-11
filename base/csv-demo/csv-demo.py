c9916d1229ffc12687418b06fab848ccec44b362
Aiden <mi_duo@live.com>
2023-07-11 22:37:52 +0800

    feat: basse

--- base/csv-demo/csv-demo.py
+++ base/csv-demo/csv-demo.py
@@ -14,8 +14,8 @@ with open('cid1.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         try:
-            id = int(row[0].strip())
-            print(f'type(id): {type(id)}')
+            cid = int(row[0].strip())
+            print(f'type(id): {type(cid)}')
         except ValueError:
             print(f'row[0]: {row[0]} is not int')
             continue
