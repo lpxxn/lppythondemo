a8d7c07571242130639d92a229d80cb59cb142ba
Aiden <mi_duo@live.com>
2023-07-11 22:35:38 +0800

    feat: basse

---
+++ base/csv-demo/csv-demo.py
@@ -0,0 +1,26 @@
+import csv
+
+cid_header = ['id']
+with open('cid.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row == cid_header:
+            print(f"header{row}")
+            continue
+        print(row)
+
+cid1_header = ['id', 'name']
+with open('cid1.csv') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        try:
+            id = int(row[0])
+            print(f'type(id): {type(id)}')
+        except ValueError:
+            print(f'row[0]: {row[0]} is not int')
+            continue
+        if row == cid1_header:
+            print(f"header{row}")
+            continue
+
+        print(f'id: {row[0]}, name: {row[1]}')
